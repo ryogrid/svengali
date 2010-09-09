cf0a8f6ee02fe351afbde8e2a933fb8b868aa69e
Ryo Kanbayashi <ryo.contact@gmail.com>
2010-09-10 00:31:59 +0900

    interpreted to English

--- svengali_sample.rb
+++ svengali_sample.rb
@@ -1,14 +1,12 @@
 #! /bin/ruby
 # -*- coding: utf-8 -*-
-require "svengali/svengali"
-
-#デフォルトはRedhat系ディストリビューションの環境を想定
-#他の環境で使う場合は以下のように指定
-#change_platform("debian")
+require "rubygems"
+require "svengali"
 
 user_name = "xxxxxxxx"
 password = "xxxxxxxx"
 
+# Use IP addres due to access by sequence number
 IPADDR_BASE = CLibIPAddr.new("xxx.xxx.xxx.xxx")
 
 MACHINE_NUM = 1
@@ -16,28 +14,30 @@ nodes = Array.new(MACHINE_NUM)
 
 tmp_ipaddr = IPADDR_BASE.dup()
 MACHINE_NUM.times{ |n|
+  # FQDN machine name can be also passed
   nodes[n] = Machine.new(tmp_ipaddr)
   nodes[n].set_auth_info(user_name,password)
-  #コマンドの発行が可能な通信路を確保する
+  # establishes a transport for command execution via sshd_conf
   nodes[n].establish_session()
   puts nodes[n].exec!("uname -v")
-  #change_platformしていない場合はyumが利用される
+  # install package
+  # package system used is default one ( yum )
   puts nodes[n].install_package("xxxx")
   tmp_ipaddr.inc!()
 }
 
 nodes.each{ |a_node|
-    #設定ファイルを編集
-    sshd_conf = a_node.get_config_file("xxxxxxx.xxx")
-    sshd_conf.replace_col("xxxxxx","xxxxxx")
-    sshd_conf.save()
+  # edit configuration file
+  sshd_conf = a_node.get_config_file("xxxxxxx.conf")
+  sshd_conf.replace_col("xxxxxx","xxxxxx")
+  sshd_conf.save()
 }
 
-##実験開始
+# start experiment
 nodes.each{ |a_node|
-   #スクリプトを転送
-   a_node.push_a_file("./xxxx.sh","/home/xxx/xxxx.sh")
-
-   #スクリプトを実行
-   puts a_node.exec_script_on("/home/xxx/xxxx.sh,"","/home/xxx")
+  # bonus
+  puts a_node.exec!("uname -a")
+  
+  # executes experiment script_on
+  puts a_node.exec_script_on("/home/xxx/xxxx.sh,"","/home/xxx")
 }
